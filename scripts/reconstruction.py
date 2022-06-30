5d4e885a650cf431d8f8dbdca06470e056a3ac5b
gordeev.al <aleksandr_gordeev2@epam.com>
2022-06-30 15:27:02 +0300

    remove global config and add params file

--- scripts/reconstruction.py
+++ scripts/reconstruction.py
@@ -1,15 +1,16 @@
 import os
 import sys
 import inspect
-
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-import json
+import yaml
+import click
 import torch
-import argparse
+from pathlib import Path
 import nvdiffrast.torch as dr
+from argparse import Namespace
 from nvdiffrec.render import obj
 from nvdiffrec.render import light
 from nvdiffrec.geometry.dlmesh import DLMesh
@@ -21,48 +22,38 @@ from nvdiffrec.support.validation_and_testing import validate
 from nvdiffrec.support.material_utility import initial_guess_material
 
 RADIUS = 3.0
+ROOT = Path(__file__).parent
 
 
-def set_flags():
-    parser = argparse.ArgumentParser(description="nvdiffrec")
-    parser.add_argument("--config", type=str, default=None, help="Config file")
-    parser.add_argument("-i", "--iter", type=int, default=5000)
-    parser.add_argument("-b", "--batch", type=int, default=1)
-    parser.add_argument("-s", "--spp", type=int, default=1)
-    parser.add_argument("-l", "--layers", type=int, default=1)
-    parser.add_argument("-r", "--train-res", nargs=2, type=int, default=[512, 512])
-    parser.add_argument("-dr", "--display-res", type=int, default=None)
-    parser.add_argument("-tr", "--texture-res", nargs=2, type=int, default=[1024, 1024])
-    parser.add_argument("-di", "--display-interval", type=int, default=0)
-    parser.add_argument("-si", "--save-interval", type=int, default=1000)
-    parser.add_argument("-lr", "--learning-rate", type=float, default=0.01)
-    parser.add_argument("-mr", "--min-roughness", type=float, default=0.08)
-    parser.add_argument("-mip", "--custom-mip", action="store_true", default=False)
-    parser.add_argument("-rt", "--random-textures", action="store_true", default=False)
-    parser.add_argument(
-        "-bg",
-        "--background",
-        default="checker",
-        choices=["black", "white", "checker", "reference"],
-    )
-    parser.add_argument(
-        "--loss", default="logl1", choices=["logl1", "logl2", "mse", "smape", "relmse"]
-    )
-    parser.add_argument("-o", "--out-dir", type=str, default=None)
-    parser.add_argument("-rm", "--ref_mesh", type=str)
-    parser.add_argument("-bm", "--base-mesh", type=str, default=None)
-    parser.add_argument("--validate", type=bool, default=True)
-
-    FLAGS = parser.parse_args()
-
+def set_flags(ref_mesh, out_dir):
+    FLAGS = Namespace()
+    FLAGS.iter = 5000
+    FLAGS.batch = 1
+    FLAGS.spp = 1
+    FLAGS.layers = 1
+    FLAGS.train_res = [512, 512]
+    FLAGS.display_res = None
+    FLAGS.texture_res = [1024, 1024]
+    FLAGS.display_interval = 0
+    FLAGS.save_interval = 500
+    FLAGS.learning_rate = 0.01
+    FLAGS.min_roughness = 0.08
+    FLAGS.custom_mip = True
+    FLAGS.random_textures = True
+    FLAGS.background = "white"
+    FLAGS.loss = "logl1"
+    FLAGS.out_dir = out_dir
+    FLAGS.ref_mesh = ref_mesh
+    FLAGS.base_mesh = None
+    FLAGS.validate = True
     FLAGS.mtl_override = None  # Override material of model
     FLAGS.dmtet_grid = 256  # Resolution of initial tet grid.
     FLAGS.mesh_scale = 2.5  # Scale of tet grid box. Adjust to cover the model
     FLAGS.env_scale = 1.0  # Env map intensity multiplier
     FLAGS.envmap = None  # HDR environment probe
-    FLAGS.display = (
-        None  # Conf validation window/display. E.g. [{"relight" : <path to envlight>}]
-    )
+    FLAGS.display = [
+        {"latlong": True}, {"bsdf": "kd"}, {"bsdf": "ks"}, {"bsdf": "normal"}
+    ]
     FLAGS.camera_space_light = False  # Fixed light in camera space.
     FLAGS.lock_light = False  # Disable light optimization in the second pass
     FLAGS.lock_pos = False  # Disable vertex position optimization in the second pass
@@ -74,7 +65,7 @@ def set_flags():
     FLAGS.pre_load = True  # Pre-load entire dataset into memory for faster training
     FLAGS.kd_min = [0.0, 0.0, 0.0, 0.0]  # Limits for kd
     FLAGS.kd_max = [1.0, 1.0, 1.0, 1.0]
-    FLAGS.ks_min = [0.0, 0.08, 0.0]  # Limits for ks
+    FLAGS.ks_min = [0.0, 0.2, 0.0]  # Limits for ks
     FLAGS.ks_max = [1.0, 1.0, 1.0]
     FLAGS.nrm_min = [-1.0, -1.0, 0.0]  # Limits for normal map
     FLAGS.nrm_max = [1.0, 1.0, 1.0]
@@ -93,10 +84,16 @@ def set_flags():
         torch.cuda.set_device(FLAGS.local_rank)
         torch.distributed.init_process_group(backend="nccl", init_method="env://")
 
-    if FLAGS.config is not None:
-        data = json.load(open(FLAGS.config, "r"))
+    # save best params
+    with open(ROOT / Path("params.yaml"), "r") as stream:
+        data = yaml.safe_load(stream)["train"]
         for key in data:
             FLAGS.__dict__[key] = data[key]
+
+    # if FLAGS.config is not None:
+    #     data = json.load(open(FLAGS.config, "r"))
+    #     for key in data:
+    #         FLAGS.__dict__[key] = data[key]
 
     if FLAGS.display_res is None:
         FLAGS.display_res = FLAGS.train_res
@@ -109,11 +106,16 @@ def set_flags():
         print("---------")
 
     os.makedirs(FLAGS.out_dir, exist_ok=True)
+
     return FLAGS
 
 
-def base_run():
-    FLAGS = set_flags()
+@click.command()
+@click.option("--ref_mesh", type=str, default="data/processed/configs/", help="Config file")
+@click.option("--out_dir", type=str, default="data/results", help="Config file")
+def base_run(ref_mesh, out_dir):
+
+    FLAGS = set_flags(ref_mesh, out_dir)
 
     glctx = dr.RasterizeGLContext()
 
@@ -176,7 +178,7 @@ def base_run():
     os.makedirs(os.path.join(FLAGS.out_dir, "eval_mesh"), exist_ok=True)
     obj.write_obj(os.path.join(FLAGS.out_dir, "eval_mesh/"), eval_mesh)
 
-    return geometry, mat
+    return geometry, mat, FLAGS
 
 
 def refinement_run():
