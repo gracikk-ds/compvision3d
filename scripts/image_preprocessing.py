d7f195f4caf259e18ac8531008ca88ee8b2a6b36
Mykhailo_Tkachuk <mykhail.tkachuk@gmail.com>
2022-07-01 16:14:20 +0300

    new pipeline created

--- scripts/image_preprocessing.py
+++ scripts/image_preprocessing.py
@@ -24,9 +24,6 @@ def main():
     pass
 
 
-TRIM = [39, 2652]
-THETA_DIRECTION = 1  # apply right-hand rule
-SCALE = True
 CAMERA_ID = 0
 
 
@@ -39,23 +36,31 @@ def parse_meta(meta_):
 
     meta_["l"] = float(meta_["camera_distance"])
     meta_["h"] = float(meta_["camera_height"]) - float(meta_["stand_height"])
-    if SCALE:
-        meta_["l"] /= meta_["h"]
-        meta_["h"] /= meta_["h"]
+    meta_["l"] /= meta_["h"]
+    meta_["h"] /= meta_["h"]
+
+    meta_['theta_direction'] = int(meta_.get('theta_direction', 1))
+    trim_ = meta_.get('trim', [0, 99999999999])
+    meta_['trim'] = [int(trim_[0]), int(trim_[1])]
 
 
-def get_theta(n_fr):
+def get_theta(n_fr, lookup=None, meta_=None):
     """
     Main callable to get theta rotation angle from the frame number
     :param n_fr: int number of frame
+    :param lookup: a lookup table. if not None the thetas will be sampled from there
+    :param meta_: a metadata dict
     :return: theta angle in radians preserving direction sign
     """
-    if n_fr < TRIM[0]:
+    if lookup is not None:
+        return lookup[n_fr]
+
+    if n_fr < meta_['trim'][0]:
         return 0
-    elif n_fr > TRIM[1]:
-        return THETA_DIRECTION * 2 * np.pi
+    elif n_fr > meta_['trim'][1]:
+        return meta_['theta_direction'] * 2 * np.pi
     else:
-        return THETA_DIRECTION * 2 * np.pi * (n_fr - TRIM[0]) / (TRIM[1] - TRIM[0])
+        return meta_['theta_direction'] * 2 * np.pi * (n_fr - meta_['trim'][0]) / (meta_['trim'][1] - meta_['trim'][0])
 
 
 def get_camera_init_qt(meta_):
@@ -92,13 +97,15 @@ def rotate_by_theta(theta_, camera_position):
 @click.option("--path_to_images_folder", default="images/", type=str)
 @click.option("--amount_of_frames", default=150, type=int)
 @click.option("--metadata", default="data/raw/meta/meta.json", type=str)
+@click.option("--theta_path", default="data/raw/meta/theta.json", type=str)
 @click.option("--colmap_text_folder", default="data/processed/colmap_db/colmap_text")
 def extract_images_from_video(
-    path_to_video: str,
-    path_to_images_folder: str,
-    amount_of_frames: int,
-    metadata: str,
-    colmap_text_folder: str,
+        path_to_video: str,
+        path_to_images_folder: str,
+        amount_of_frames: int,
+        metadata: str,
+        theta_path: str,
+        colmap_text_folder: str,
 ) -> None:
     """
     Extract predefined number of images from video
@@ -106,17 +113,22 @@ def extract_images_from_video(
     @param path_to_images_folder: path to image folder
     @param amount_of_frames: desirable amount of images to extract
     @param metadata: metadata of the video
+    @param theta_path: path to json file containing theta angles per frame
     @param colmap_text_folder: folder to save colmap images
     @return: None
     """
     os.makedirs(colmap_text_folder, exist_ok=True)
-    compute_rotations = True
     if not os.path.exists(metadata):
-        compute_rotations = False
-    if compute_rotations:
-        with open(metadata) as j:
-            meta = json.load(j)
-        parse_meta(meta)
+        raise FileNotFoundError("A meta.json file is required")
+    with open(metadata) as j:
+        meta = json.load(j)
+    parse_meta(meta)
+
+    if os.path.exists(theta_path):
+        with open(theta_path) as th:
+            theta_lookup = json.load(th)
+    else:
+        theta_lookup = None
 
     print("start extract_images_from_video")
     os.makedirs(path_to_images_folder, exist_ok=True)
@@ -125,17 +137,17 @@ def extract_images_from_video(
     cam = cv2.VideoCapture(path_to_video)
 
     frame_count = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
-    reducer = (TRIM[1] - TRIM[0]) // amount_of_frames
+    reducer = (meta['trim'][1] - meta['trim'][0]) // amount_of_frames
 
     # frame
     frame_number = 0
     frame_to_write_number = 0
 
-    if compute_rotations:
-        print("yoy")
-        camera_init_pose = get_camera_init_qt(meta)
-        with open(os.path.join(colmap_text_folder, "images.txt"), "w") as out:
-            pass
+
+    print("yoy")
+    camera_init_pose = get_camera_init_qt(meta)
+    with open(os.path.join(colmap_text_folder, "images.txt"), "w") as out:
+        pass
 
     with tqdm(total=frame_count) as pbar:
         while True:
@@ -143,26 +155,26 @@ def extract_images_from_video(
             ret, frame = cam.read()
             if not ret:
                 break
-            if not TRIM[0] <= frame_number <= TRIM[1]:
+            if not meta['trim'][0] <= frame_number <= meta['trim'][1]:
                 frame_number += 1
                 continue
 
-            if (frame_number - TRIM[0]) % reducer == 0:
+            if (frame_number - meta['trim'][0]) % reducer == 0:
                 name = path_to_images_folder / f"{frame_to_write_number:03d}.jpg"
                 cv2.imwrite(str(name), frame)
-                if compute_rotations:
-                    theta = get_theta(frame_number)
-                    r, t = rotate_by_theta(theta, camera_init_pose)
-                    r = r.conjugate()  # make it a world to camera transform
-                    t = -r * t * r.conjugate()
-                    with open(
+
+                theta = get_theta(frame_number, lookup=theta_lookup, meta_=meta)
+                r, t = rotate_by_theta(theta, camera_init_pose)
+                r = r.conjugate()  # make it a world to camera transform
+                t = -r * t * r.conjugate()
+                with open(
                         os.path.join(colmap_text_folder, "images.txt"), "a"
-                    ) as out:
-                        out.write(
-                            f"{frame_to_write_number} {r.w} {r.x} {r.y} {r.z} {t.x} "
-                            f"{t.y} {t.z} {CAMERA_ID} "
-                            f"{frame_to_write_number:03d}.png\n0 0 -1\n"
-                        )  # 0 0 -1 is a placeholder
+                ) as out:
+                    out.write(
+                        f"{frame_to_write_number} {r.w} {r.x} {r.y} {r.z} {t.x} "
+                        f"{t.y} {t.z} {CAMERA_ID} "
+                        f"{frame_to_write_number:03d}.png\n0 0 -1\n"
+                    )  # 0 0 -1 is a placeholder
                 frame_to_write_number += 1
             frame_number += 1
             pbar.update()
@@ -172,8 +184,8 @@ def extract_images_from_video(
 @click.option("--path_to_images_folder", default="images/", type=str)
 @click.option("--path_to_cropped_images_folder", default="cropped_images/", type=str)
 def crop_resize_images(
-    path_to_images_folder: str,
-    path_to_cropped_images_folder: str,
+        path_to_images_folder: str,
+        path_to_cropped_images_folder: str,
 ) -> None:
     """
     Crop and resize images
@@ -192,7 +204,7 @@ def crop_resize_images(
     for image_path in tqdm(images, total=len(images)):
         image = cv2.imread(str(image_path))
 
-        image = image[250 : h - 650, delta + 450 : w - (delta + 450)]
+        image = image[250: h - 650, delta + 450: w - (delta + 450)]
 
         width = 800
         height = 800
@@ -211,7 +223,7 @@ def crop_resize_images(
 @click.option("--images_no_background", default="images_no_background/", type=str)
 @click.option("--model_type", default="new", type=click.Choice(["new", "old"]))
 def remove_background(
-    path_to_cropped_images_folder: str, images_no_background: str, model_type: str
+        path_to_cropped_images_folder: str, images_no_background: str, model_type: str
 ) -> None:
     """
     Run background removal net.
